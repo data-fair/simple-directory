f6b25ef088b2c20332021e8fc290642bed478eb6
Alban Mouton <alban.mouton@gmail.com>
2018-05-15 17:20:01 +0200

    feat: webhooks send only 1 name

--- server/webhooks.js
+++ server/webhooks.js
@@ -3,13 +3,17 @@ const axios = require('axios')
 const userName = require('./utils/user-name')
 
 exports.sendUsersWebhooks = async (users) => {
-  for (let webhook of config.webhooks.updateEntityNames) {
-    await axios.post(webhook, users.map(user => ({type: 'user', id: user.id, name: userName(user)})))
+  for (let user of users) {
+    for (let webhook of config.webhooks.updateEntityNames) {
+      await axios.post(webhook, {type: 'user', id: user.id, name: userName(user)})
+    }
   }
 }
 
 exports.sendOrganizationsWebhooks = async (organizations) => {
-  for (let webhook of config.webhooks.updateEntityNames) {
-    await axios.post(webhook, organizations.map(organization => ({type: 'organization', id: organization.id, name: organization.name})))
+  for (let organization of organizations) {
+    for (let webhook of config.webhooks.updateEntityNames) {
+      await axios.post(webhook, {type: 'organization', id: organization.id, name: organization.name})
+    }
   }
 }
