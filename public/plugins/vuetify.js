bfaa3eb7e837a1821a0ec889c2124a00e38120e3
Alban Mouton <alban.mouton@gmail.com>
2018-06-04 17:26:01 +0200

    fix: application of dynamic vuetify theme

--- public/plugins/vuetify.js
+++ public/plugins/vuetify.js
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-export default ({store}) => {
-  if (store.state.env.theme) {
-    Vue.use(Vuetify, {
-      theme: store.state.env.theme.colors
-    })
-  } else {
-    Vue.use(Vuetify)
-  }
+export default ({env}) => {
+  Vue.use(Vuetify, {theme: env.theme.colors})
 }
