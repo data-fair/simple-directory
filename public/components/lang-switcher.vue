b90424740f942b49a75cb4aada3c8c30a6e8bb55
Alban Mouton <alban.mouton@gmail.com>
2018-06-04 17:45:45 +0200

    fix: visible lang switcher

--- public/components/lang-switcher.vue
+++ public/components/lang-switcher.vue
@@ -3,7 +3,7 @@
     direction="bottom"
     transition="fade-transition"
   >
-    <v-btn slot="activator" fab flat small>{{ $i18n.locale }}</v-btn>
+    <v-btn slot="activator" fab depressed small>{{ $i18n.locale }}</v-btn>
     <v-btn v-for="locale in $i18n.locales.filter(l => l.code !== $i18n.locale)" :key="locale.code" :to="switchLocalePath(locale.code)" fab small nuxt>
       {{ locale.code }}
     </v-btn>
