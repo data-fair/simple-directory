c874272d36d0078549e2ffd6dcb364a23f5894e8
Alban Mouton <alban.mouton@gmail.com>
2020-04-16 15:46:07 +0200

    fix: broken test after adding avatarUrl

--- test/users.js
+++ test/users.js
@@ -14,7 +14,7 @@ test('Get user list when authenticated', async t => {
   const res = await ax.get('/api/users')
   t.is(res.status, 200)
   t.is(res.data.count, 11)
-  t.deepEqual(Object.keys(res.data.results[0]), ['id', 'name'])
+  t.deepEqual(Object.keys(res.data.results[0]), ['id', 'name', 'avatarUrl'])
 })
 
 test('Get filtered user list', async t => {
