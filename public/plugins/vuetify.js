2dd999727e27e9765878285baebb6cfc2d33578e
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2021-01-21 11:36:02 +0100

    feat: can change primary color with query param

--- public/plugins/vuetify.js
+++ public/plugins/vuetify.js
@@ -6,9 +6,13 @@ require('../stylus/main.styl')
 export default ({ env, app, route }) => {
   if (app.$cookies.get('theme_dark') !== undefined) env.theme.dark = app.$cookies.get('theme_dark')
   if (route.query.dark) env.theme.dark = route.query.dark === 'true'
+  const theme = env.theme.dark ? { ...env.theme.colors, ...env.theme.darkColors } : env.theme.colors
+  if (route.query && route.query.primary) {
+    theme.primary = route.query.primary
+  }
 
   Vue.use(Vuetify, {
     iconfont: 'mdi',
-    theme: env.theme.dark ? { ...env.theme.colors, ...env.theme.darkColors } : env.theme.colors
+    theme
   })
 }
