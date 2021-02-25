54d79d3745dd67c4de3677fa6504873256a7e861
Alban Mouton <alban.mouton@gmail.com>
2021-02-25 14:31:51 +0100

    fix: broken api of http-proxy-middleware

--- server/nuxt.js
+++ server/nuxt.js
@@ -1,11 +1,11 @@
 const { Nuxt } = require('nuxt')
-
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const nuxtConfig = require('../nuxt.config.js')
 
 module.exports = async () => {
   if (['development', 'test'].includes(process.env.NODE_ENV)) {
     // in dev mode the nuxt dev server is already running, we re-expose it
-    return require('http-proxy-middleware')({ target: 'http://localhost:3000' })
+    return createProxyMiddleware({ target: 'http://localhost:3000' })
   } else {
     // Prepare nuxt for rendering and serving UI
     const nuxt = new Nuxt(nuxtConfig)
