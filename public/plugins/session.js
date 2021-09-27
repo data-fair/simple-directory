6bc0a62e830c03a24086333a73c852f000ad08f1
Alban Mouton <alban.mouton@gmail.com>
2021-09-27 11:47:03 +0200

    Revert "chore: debug"
    This reverts commit 1f20e7e3b542d1e710bcfc831b2365de19d8490f.

--- public/plugins/session.js
+++ public/plugins/session.js
@@ -1,7 +1,5 @@
 export default ({ store, app, env, req }) => {
   store.commit('setAny', { env: { ...env } })
-  console.log('basepath', env.basePath)
-  console.log('directoryUrl', env.basePath.slice(0, -1))
   store.dispatch('session/init', {
     cookies: app.$cookies,
     directoryUrl: env.basePath.slice(0, -1),
