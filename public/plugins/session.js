6ed218b3234da976a3cc226829e432621bf73c88
Alban Mouton <alban.mouton@gmail.com>
2021-09-27 11:47:31 +0200

    fix: bad sirectoryUrl given to sd-vue

--- public/plugins/session.js
+++ public/plugins/session.js
@@ -2,7 +2,7 @@ export default ({ store, app, env, req }) => {
   store.commit('setAny', { env: { ...env } })
   store.dispatch('session/init', {
     cookies: app.$cookies,
-    directoryUrl: env.basePath.slice(0, -1),
+    directoryUrl: '', // will use browserBaseUrl from nuxt.config.js
   })
   store.dispatch('session/loop', app.$cookies)
 }
