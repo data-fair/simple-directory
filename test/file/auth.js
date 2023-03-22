b647c55e66805980a6186f4a842d733ad8571642
Alban Mouton <alban.mouton@gmail.com>
2023-03-22 16:47:43 +0100

    chore: add debug log

--- test/file/auth.js
+++ test/file/auth.js
@@ -3,6 +3,9 @@ const testUtils = require('../utils')
 
 describe('auth', () => {
   it('Create session for known user', async () => {
+    const apiDocUrl = `${require('config').publicUrl}/api/api-docs.json`
+    const apiDoc = await require('axios').get(apiDocUrl)
+    console.log('api doc ' + apiDocUrl, apiDoc)
     const ax = await testUtils.axios('dmeadus0@answers.com:testpasswd')
     const res = await ax.get('/api/auth/me')
     assert.equal(res.status, 200)
