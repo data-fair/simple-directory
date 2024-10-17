8b545c14afa9840922700bbdd5cff23bc0f9dfb3
Alban Mouton <alban.mouton@gmail.com>
2024-10-17 14:27:17 +0200

    refactor: typed anonymous actions router

--- api/src/anonymous-action/router.ts
+++ api/src/anonymous-action/router.ts
@@ -1,11 +1,14 @@
+import { type Request, type Response, type NextFunction } from 'express'
 import config from '#config'
-const limiter = require('../utils/limiter')
-const requestIp = require('request-ip')
+import { reqIp } from '@data-fair/lib-express'
+import { reqI18n } from '#i18n'
+import limiter from '../utils/limiter.ts'
+
 const tokens = require('../utils/tokens')
 
 // simply get a token to perform an anonymous action in the close future
 // useful to ensure that the user is human and waits for a little while before submitting a form
-export default  async (req, res, next) => {
+export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     await limiter(req).consume(reqIp(req), 1)
   } catch (err) {
