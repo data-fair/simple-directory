28789b4f36ba77356e4d70cd7802c9dc22c7328f
Alban Mouton <alban.mouton@gmail.com>
2024-10-21 15:54:58 +0200

    refactor: types of limiter utils

---
+++ api/src/utils/limiter.ts
@@ -0,0 +1,20 @@
+import { RateLimiterMongo } from 'rate-limiter-flexible'
+import { type Request } from 'express'
+import config from '#config'
+import mongo from '#mongo'
+
+// protect authentication routes with rate limiting to prevent brute force attacks
+let _limiter
+const limiterOptions = {
+  keyPrefix: 'sd-rate-limiter-auth',
+  points: config.authRateLimit.attempts,
+  duration: config.authRateLimit.duration
+}
+export default (req: Request) => {
+  _limiter = _limiter || new RateLimiterMongo({
+    storeClient: mongo.client,
+    dbName: mongo.db.databaseName,
+    ...limiterOptions
+  })
+  return _limiter
+}
