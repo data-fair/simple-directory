86d2da5e7adfd242951ce8dc3179e4ad260c0ecf
Alban Mouton <alban.mouton@gmail.com>
2024-10-21 17:12:43 +0200

    refactor: types of limits router

--- api/src/limits/router.ts
+++ api/src/limits/router.ts
@@ -1,27 +1,22 @@
 import config from '#config'
-import { Router } from 'express'
-import { reqUser } from '@data-fair/lib-express'
+import { Router, type Request, type Response, type NextFunction } from 'express'
+import { reqUser, reqSessionAuthenticated, assertAccountRole, httpError, type Account } from '@data-fair/lib-express'
+import * as limitsSchema from '#types/limits/index.ts'
+import mongo from '#mongo'
 
 const router = Router()
+export default router
 
-const isSuperAdmin = (req, res, next) => {
+const isSuperAdmin = (req: Request, res: Response, next: NextFunction) => {
   if (reqUser(req)?.adminMode) return next()
   if (req.query.key && req.query.key === config.secretKeys.limits) return next()
   res.status(401).send()
 }
 
-const isAccountMember = (req, res, next) => {
+const isAccountMember = (req: Request, res: Response, next: NextFunction) => {
   if (req.query.key && req.query.key === config.secretKeys.limits) return next()
-  if (!reqUser(req)) return res.status(401).send()
-  if (reqUser(req)?.adminMode) return next()
-  if (!['organization', 'user'].includes(req.params.type)) return res.status(400).send('Wrong consumer type')
-  if (req.params.type === 'user') {
-    if (reqUser(req).id !== req.params.id) return res.status(403).send()
-  }
-  if (req.params.type === 'organization') {
-    const org = reqUser(req).organizations.find(o => o.id === req.params.id)
-    if (!org) return res.status(403).send()
-  }
+  const session = reqSessionAuthenticated(req)
+  assertAccountRole(session, req.params as unknown as Account, 'admin', { acceptDepAsRoot: config.depAdminIsOrgAdmin })
   next()
 }
 
@@ -29,25 +24,22 @@ const isAccountMember = (req, res, next) => {
 router.post('/:type/:id', isSuperAdmin, async (req, res, next) => {
   req.body.type = req.params.type
   req.body.id = req.params.id
-  const valid = validate(req.body)
-  if (!valid) return res.status(400).send(validate.errors)
-  await storages.globalStorage.mongo.limits
-    .replaceOne({ type: req.params.type, id: req.params.id }, req.body, { upsert: true })
-  res.send(req.body)
+  const limits = limitsSchema.returnValid(req.body)
+  await mongo.limits
+    .replaceOne({ type: req.params.type, id: req.params.id }, limits, { upsert: true })
+  res.send(limits)
 })
 
 // A user can get limits information for himself only
 router.get('/:type/:id', isAccountMember, async (req, res, next) => {
-  const limit = await export const  getLimits(storages.globalStorage.{ type: req.params.type, id: req.params.id })
-  delete limit._id
-  res.send(limit)
+  const limits = await mongo.limits.findOne({ type: req.params.type, id: req.params.id }, { projection: { _id: 0 } })
+  if (!limits) return httpError(404)
+  res.send(limits)
 })
 
 router.get('/', isSuperAdmin, async (req, res, next) => {
-  const filter = {}
-  if (req.query.type) filter.type = req.query.type
-  if (req.query.id) filter.id = req.query.id
-  const results = await storages.globalStorage.mongo.limits
+  const filter = { type: req.query.type, id: req.query.id }
+  const results = await mongo.limits
     .find(filter)
     .sort({ lastUpdate: -1 })
     .project({ _id: 0 })
