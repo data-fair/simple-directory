021c0ba4bde9f44a9763823f3d7522141b25e5d9
Alban Mouton <alban.mouton@gmail.com>
2024-10-23 14:44:38 +0200

    refactor: work on express app init

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,10 @@
-import config from '#config'
+import { resolve } from 'node:path'
+import config, { uiConfig } from '#config'
 import apiDocs from '../contract/api-docs.ts'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import { session, errorHandler } from '@data-fair/lib-express'
+import { session, errorHandler, createSiteMiddleware, createSpaMiddleware } from '@data-fair/lib-express'
 import admin from './admin/router.ts'
 import anonymousAction from './anonymous-action/router.ts'
 import auth from './auth/router.ts'
@@ -16,14 +17,20 @@ import invitations from './invitations/router.ts'
 import avatars from './avatars/router.ts'
 import oauthTokens from './oauth-tokens/router.ts'
 import tokens from './tokens/router.ts'
+import sites from './sites/router.ts'
 
 const app = express()
 export default app
 
+// no fancy embedded arrays, just string and arrays of strings in req.query
+app.set('query parser', 'simple')
 app.set('json spaces', 2)
 
 app.use(cookieParser())
 app.use(bodyParser.json({ limit: '100kb' }))
+
+app.use(createSiteMiddleware('simple-directory'))
+app.use(session.middleware())
 
 // Replaces reqUser(req) from session with full and fresh user object from storage
 // also minimalist api key management
@@ -57,22 +64,22 @@ app.use(bodyParser.json({ limit: '100kb' }))
 app.get('/api/api-docs.json', (req, res) => res.send(apiDocs))
 app.get('/api/admin', admin)
 app.get('/api/auth/anonymous-action', anonymousAction)
-app.use('/api/auth', session.middleware(), auth)
-app.use('/api/mails', session.middleware(), mails)
-app.use('/api/users', session.middleware(), users)
-app.use('/api/organizations', session.middleware(), organizations)
-app.use('/api/invitations', session.middleware(), invitations)
-app.use('/api/avatars', session.middleware(), avatars)
+app.use('/api/auth', auth)
+app.use('/api/mails', mails)
+app.use('/api/users', users)
+app.use('/api/organizations', organizations)
+app.use('/api/invitations', invitations)
+app.use('/api/avatars', avatars)
 app.use('/api/limits', auth, limits)
 app.use('/api/2fa', twoFA)
-app.use('/api/oauth-tokens', session.middleware(), oauthTokens)
-if (config.manageSites) {
-  app.use('/api/sites', session.middleware(), (await import('./sites/router.ts')).default)
-}
+app.use('/api/oauth-tokens', oauthTokens)
+if (config.manageSites) app.use('/api/sites', sites)
 
 app.use('/api/', (req, res) => {
   return res.status(404).send('unknown api endpoint')
 })
 app.use(tokens)
+
+// app.use(await createSpaMiddleware(resolve(import.meta.dirname, '../../ui/dist'), uiConfig))
 
 app.use(errorHandler)
