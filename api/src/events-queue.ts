a33a5daa58674eabe1fbae6999c6bf98dfe5db40
Alban Mouton <alban.mouton@gmail.com>
2025-01-06 16:07:26 +0100

    chore: debug events queue init

--- api/src/events-queue.ts
+++ api/src/events-queue.ts
@@ -1,5 +1,8 @@
 import { EventsQueue } from '@data-fair/lib-node/events-queue.js'
 import config from '#config'
+import Debug from 'debug'
+
+const debug = Debug('events-queue')
 
 let eventsQueue: EventsQueue | undefined
 
@@ -8,7 +11,10 @@ export default eventsQueue
 export async function start () {
   if (config.secretKeys.events && config.privateEventsUrl) {
     eventsQueue = new EventsQueue()
+    debug('start events queue', config.privateEventsUrl)
     await eventsQueue.start({ eventsUrl: config.privateEventsUrl, eventsSecret: config.secretKeys.events })
+  } else {
+    debug('events queue not started because of missing config')
   }
 }
 
