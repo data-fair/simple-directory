a52eee63b96fd2809b541c71a494357dab656b09
Alban Mouton <alban.mouton@gmail.com>
2025-01-08 15:02:28 +0100

    fix: missing patchable properties for admins

--- api/doc/users/patch-req/schema.js
+++ api/doc/users/patch-req/schema.js
@@ -3,8 +3,9 @@ import UserSchema from '#types/user/schema.js'
 
 const body = jsonSchema(UserSchema)
   .removeReadonlyProperties()
-  .pickProperties(['firstName', 'lastName', 'birthday', 'ignorePersonalAccount', 'defaultOrg', 'defaultDep', 'plannedDeletion'])
-  .makeNullable(['firstName', 'lastName', 'birthday', 'ignorePersonalAccount', 'defaultOrg', 'defaultDep', 'plannedDeletion'])
+  .pickProperties(['firstName', 'lastName', 'birthday', 'ignorePersonalAccount', 'defaultOrg', 'defaultDep', 'plannedDeletion', 'maxCreatedOrgs', 'email', '2FA'])
+  .makeNullable(['firstName', 'lastName', 'birthday', 'ignorePersonalAccount', 'defaultOrg', 'defaultDep', 'plannedDeletion', 'maxCreatedOrgs', '2FA'])
+  .removeFromRequired(['email'])
   .removeId()
   .appendTitle(' patch')
   .schema
