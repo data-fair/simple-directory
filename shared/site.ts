2bf9dc243d4bf82985d41eeea31be4b82c55f237
Alban Mouton <alban.mouton@gmail.com>
2025-01-08 11:38:06 +0100

    chore: add transparent background rule in print mode

--- shared/site.ts
+++ shared/site.ts
@@ -8,9 +8,7 @@ export const getTextColorsCss = (colors: Colors, theme: string) => {
     const key = `text-${color}` as keyof Colors
     if (colors[key]) {
       css += `
-.v-theme--${theme} .text-${color} {
-  color: ${colors[key]}!important;
-}`
+.v-theme--${theme} .text-${color} { color: ${colors[key]}!important; }`
     }
   }
   return css
