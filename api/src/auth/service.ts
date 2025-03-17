6f3c4f58315656a4db8d9fec1a49714e666fb7a7
Alban Mouton <alban.mouton@gmail.com>
2025-03-17 16:31:39 +0100

    feat: add site sso configuration to put new members in a department

--- api/src/auth/service.ts
+++ api/src/auth/service.ts
@@ -8,19 +8,20 @@ import type { Site, User, Organization, ServerSession, UserWritable } from '#typ
 import storages from '#storages'
 import eventsQueue from '#events-queue'
 import { nanoid } from 'nanoid'
-import type { CreateMember, AuthProvider, MemberRole, OpenIDConnect } from '#types/site/index.ts'
+import type { CreateMember, AuthProvider, MemberRole, MemberDepartment, OpenIDConnect } from '#types/site/index.ts'
 import { getOrgLimits, getRedirectSite, getTokenPayload, prepareCallbackUrl, reqSite, setNbMembersLimit, unshortenInvit } from '#services'
 import { __all, reqI18n } from '#i18n'
 
 const debugAuthProvider = debugModule('auth-provider')
 
-type AuthProviderMemberInfo = { create: boolean, org?: Organization, readOnly: boolean, role: string }
+type AuthProviderMemberInfo = { create: boolean, org?: Organization, readOnly: boolean, role: string, department?: string }
 
 type AuthProviderCore = {
   id: string,
   type: 'saml2' | 'oidc' | 'oauth',
   createMember?: CreateMember,
   memberRole?: MemberRole,
+  memberDepartment?: MemberDepartment,
   coreIdProvider?: boolean
 }
 
@@ -56,17 +57,30 @@ export const authProviderMemberInfo = async (site: Site | undefined, provider: A
 
   let role = 'user'
   let readOnly = false
-  if (provider.coreIdProvider && provider.memberRole && provider.memberRole?.type !== 'none') {
+  if (provider.coreIdProvider && ((provider.memberRole && provider.memberRole?.type !== 'none') || (provider.memberDepartment && provider.memberDepartment?.type !== 'none'))) {
     readOnly = true
   }
   if (provider.memberRole?.type === 'static') {
     role = provider.memberRole.role
   }
   if (provider.memberRole?.type === 'attribute') {
-    role = authInfo.data[provider.memberRole.attribute]
+    role = authInfo.data[provider.memberRole.attribute] ?? role
+  }
+  let department
+  if (provider.memberDepartment?.type === 'static') {
+    department = provider.memberDepartment.department
+  }
+  if (provider.memberDepartment?.type === 'attribute') {
+    department = authInfo.data[provider.memberDepartment.attribute] as string | undefined
+    if (!department && provider.memberDepartment.required) {
+      throw httpError(400, "l'attribute de département n'est pas défini et est obligatoire")
+    }
+    if (provider.memberDepartment.orgRootValue && department === provider.memberDepartment.orgRootValue) {
+      department = undefined
+    }
   }
 
-  return { create, org, readOnly, role }
+  return { create, org, readOnly, role, department }
 }
 
 export const authProviderLoginCallback = async (
@@ -159,7 +173,7 @@ export const authProviderLoginCallback = async (
     if (memberInfo.create && memberInfo.org) {
       logContext.account = { type: 'organization', ...memberInfo.org }
       eventsLog.info('sd.auth.provider.create-member', `a user was added as a member in oauth callback ${user.id} / ${memberInfo.role}`, logContext)
-      await storage.addMember(memberInfo.org, user, memberInfo.role, null, memberInfo.readOnly)
+      await storage.addMember(memberInfo.org, user, memberInfo.role, memberInfo.department, memberInfo.readOnly)
       await setNbMembersLimit(memberInfo.org.id)
     }
   } else {
@@ -221,9 +235,9 @@ export const patchCoreAuthUser = async (provider: AuthProviderCore, user: User, 
       if (memberInfo.create) {
         patch.defaultOrg = memberInfo.org.id
         patch.ignorePersonalAccount = true
-        await storages.globalStorage.addMember(memberInfo.org, user, memberInfo.role, null, memberInfo.readOnly)
+        await storages.globalStorage.addMember(memberInfo.org, user, memberInfo.role, memberInfo.department, memberInfo.readOnly)
       } else {
-        await storages.globalStorage.removeMember(memberInfo.org.id, user.id)
+        await storages.globalStorage.removeMember(memberInfo.org.id, user.id, '*')
       }
       await setNbMembersLimit(memberInfo.org.id)
     }
