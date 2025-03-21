182507a96bbedef515cb17186a5df6603342e4a9
Alban Mouton <alban.mouton@gmail.com>
2025-03-21 09:53:34 +0100

    fix: missing SITE_ADMIN env var

--- api/config/custom-environment-variables.cjs
+++ api/config/custom-environment-variables.cjs
@@ -311,5 +311,5 @@ module.exports = {
   passwordUpdateInterval: jsonEnv('PASSWORD_UPDATE_INTERVAL'),
   passwordValidateOnLogin: 'PASSWORD_VALIDATE_ON_LOGIN',
   siteOrgs: 'SITE_ORGS',
-  siteUsers: 'SITE_USERS'
+  siteAdmin: 'SITE_ADMIN'
 }
